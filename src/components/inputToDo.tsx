1600e986adfa3042f6165f90568a2eb4d01b1d14
Zahra <bghzahra@outlook.com>
2022-05-02 20:37:39 +0100

    added state for description and used onchange handler

--- src/components/inputToDo.tsx
+++ src/components/inputToDo.tsx
@@ -1,16 +1,23 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function InputToDo (): JSX.Element {
+export default function InputToDo(): JSX.Element {
+  const [description, setDescription] = useState("");
 
-    return (
-        <>
-        <h1 className="text-center m-4">PERN To Do List</h1>
-        <div className="form">
+  return (
+    <>
+      <h1 className="text-center m-4">PERN To Do List</h1>
+      <div className="form">
         <form className="d-flex">
-            <input type="text" className="form-control"></input>
-            <button className="btn btn-success">Add</button>
+          <input
+            type="text"
+            className="form-control"
+            value={description}
+            placeholder="Input your To Do here..."
+            onChange={e => setDescription(e.target.value)}
+          ></input>
+          <button className="btn btn-success">Add</button>
         </form>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
