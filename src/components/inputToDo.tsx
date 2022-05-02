cfb2746b32d71507ea583de26c6d841a423cb953
Zahra <bghzahra@outlook.com>
2022-05-02 20:26:38 +0100

    added form

--- src/components/inputToDo.tsx
+++ src/components/inputToDo.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react"
+
 export default function InputToDo (): JSX.Element {
+
     return (
         <>
-        <h1 className="input-title">Input To Do</h1>
+        <h1 className="text-center m-4">PERN To Do List</h1>
+        <div className="form">
+        <form className="d-flex">
+            <input type="text" className="form-control"></input>
+            <button className="btn btn-success">Add</button>
+        </form>
+        </div>
         </>
     )
 }
