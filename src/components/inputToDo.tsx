927660d43aa2474ad955dd64c54818b0133fb229
Zahra <bghzahra@outlook.com>
2022-05-03 18:38:11 +0100

    added submit form using fetch

--- src/components/inputToDo.tsx
+++ src/components/inputToDo.tsx
@@ -3,11 +3,26 @@ import { useState } from "react";
 export default function InputToDo(): JSX.Element {
   const [description, setDescription] = useState("");
 
+  const onSubmitForm = async (e:any) => {
+    e.preventDefault()
+    try {
+    const body = {description}
+    const response = await fetch('http://localhost:5000/todolist', {
+        method: "POST",
+        headers: {"Content-type":"application/json" },
+        body: JSON.stringify(body)
+    })
+        console.log(response)
+    } catch (err:any) {
+        console.error(err.message)
+    }
+  }
+
   return (
     <>
       <h1 className="text-center m-4">PERN To Do List</h1>
       <div className="form">
-        <form className="d-flex">
+        <form className="d-flex" onSubmit={onSubmitForm}>
           <input
             type="text"
             className="form-control"
