71c29b37338063141f7929b6057569b09a3d817c
Zahra <bghzahra@outlook.com>
2022-05-03 20:11:41 +0100

    fixed map function bug

---
+++ src/components/ToDosInt.ts
@@ -0,0 +1,4 @@
+export default interface toDos {
+    id:number, 
+    description:string
+}
--- src/components/allToDos.tsx
+++ src/components/allToDos.tsx
@@ -1,26 +1,27 @@
 import { useState, useEffect } from "react";
+import toDos from "./ToDosInt";
  
 export default function AllToDos(): JSX.Element {
-  const[toDos, setToDos] = useState([])
+  const[toDos, setToDos] = useState<toDos[]>([])
 
+  useEffect(() => {
   const getToDos = async () => {
     try {
       const response = await fetch("http://localhost:5000/todolist")
       const jsonData = await response.json()
-
-      setToDos(jsonData)
+ 
+      setToDos(jsonData.data)
     } catch (err:any) {
       console.error(err.message)
     }
   }
+    getToDos();
+ }, [])
 
-    useEffect(() => {
-      getToDos();
-    }, [])
 
-    console.log(toDos)
   return (
     <>
+    { console.log(toDos)}
       <div className="all-to-dos-container">
         <h3>Previous To Do's</h3>
         <table className="table">
@@ -32,11 +33,13 @@ export default function AllToDos(): JSX.Element {
       </tr>
     </thead>
     <tbody>
-      {/* <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr> */}
+      {toDos.map(toDo => (
+        <tr key={toDo.id}>
+          <td>{toDo.description}</td>
+          <td>Edit</td>
+          <td>Delete</td>
+        </tr>
+      ))}
     </tbody>
   </table>
       </div>
